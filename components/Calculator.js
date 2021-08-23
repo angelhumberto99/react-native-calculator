23bbcf04c91879fe1b331cde22eeed037398efbc
angelhumberto99 <andre.gutierrez99@gmail.com>
2021-08-23 15:21:15 -0500

    functionality has been implemented

--- components/Calculator.js
+++ components/Calculator.js
@@ -16,15 +16,97 @@ class calculator extends Component {
   operLayout = ['/', '*', '-', '+'];
 
   state = {
-    screen: '0',
+    screen: '',
+    period: false,
   };
 
   onClick = e => {
-    this.setState({screen: e});
+    let length = this.state.screen.length;
+    let lastInput = this.state.screen[length - 1];
+
+    // borra el estado
+    if (e === 'AC') this.setState({screen: ''});
+    // elimina la última entrada
+    else if (e === 'DELETE')
+      this.setState({screen: this.state.screen.slice(0, -1)});
+    // calcula el resultado
+    else if (e === '=') {
+      if (length > 0) {
+        let res = this.state.screen;
+
+        if (length === 1 && res.match(/[\-|+]/)) res = '';
+        else if (lastInput.match(/[\-|/|+|*]/)) {
+          res = res.slice(0, -1);
+          if (res[res.length - 1].match(/[\-|/|+|*]/)) res = res.slice(0, -1);
+          res = String(eval(res));
+        } else res = String(eval(res));
+        if (res === '0') res = '';
+        this.setState({screen: res});
+      }
+    }
+
+    // establece las entradas validas para las operaciones
+    else if (e.match(/[\-|/|+|*]/)) this.validateOperation(e);
+    // permite ingresar cualquier número
+    else {
+      // evalua que los numeros ingresados no tengan más de un punto decimal
+      if (!this.state.period || e !== '.')
+        this.setState({screen: this.state.screen + e});
+      if (e === '.') this.setState({period: true});
+    }
   };
 
-  getLayout = () => {
-    let numPad = this.mainLayout.map((e, i) => {
+  validateOperation = e => {
+    let length = this.state.screen.length;
+    let lastInput = this.state.screen[length - 1];
+
+    this.setState({period: false});
+    // al inicio de la operacion puede haber un + o un -
+    if (length === 0) {
+      if (e.match(/[\-|+]/)) this.setState({screen: e});
+    } else {
+      // examina si la última entrada fue una operación
+      if (lastInput.match(/[\-|/|+|*]/)) {
+        if (length === 1) {
+          if (e.match(/[\-|+]/))
+            this.setState({screen: this.state.screen.slice(0, -1) + e});
+        } else {
+          if (e.match(/[\-|+]/) && lastInput.match(/[/|*]/))
+            this.setState({screen: this.state.screen + e});
+          else if (e.match(/[*|/]/) && lastInput.match(/[\-|+]/)) {
+            if (!this.state.screen[length - 2].match(/[*|/]/))
+              this.setState({screen: this.state.screen.slice(0, -1) + e});
+          } else this.setState({screen: this.state.screen.slice(0, -1) + e});
+        }
+      } else this.setState({screen: this.state.screen + e});
+    }
+  };
+
+  createOperPad = () => {
+    return this.operLayout.map((e, i) => {
+      if (e.match(/[+]/))
+        return (
+          <CustomBtn
+            key={i}
+            val={e}
+            keyStyle={[kStyles.spetialBtn, kStyles.operKey]}
+            handler={this.onClick}
+          />
+        );
+      else
+        return (
+          <CustomBtn
+            key={i}
+            val={e}
+            keyStyle={[kStyles.defaultBtn, kStyles.operKey]}
+            handler={this.onClick}
+          />
+        );
+    });
+  };
+
+  createNumPad = () => {
+    return this.mainLayout.map((e, i) => {
       let row = e.map((e, i) => {
         if (e.match(/[AC|DELETE]/))
           return (
@@ -51,27 +133,11 @@ class calculator extends Component {
         </View>
       );
     });
+  };
 
-    let operPad = this.operLayout.map((e, i) => {
-      if (e.match(/[+]/))
-        return (
-          <CustomBtn
-            key={i}
-            val={e}
-            keyStyle={[kStyles.spetialBtn, kStyles.operKey]}
-            handler={this.onClick}
-          />
-        );
-      else
-        return (
-          <CustomBtn
-            key={i}
-            val={e}
-            keyStyle={[kStyles.defaultBtn, kStyles.operKey]}
-            handler={this.onClick}
-          />
-        );
-    });
+  getLayout = () => {
+    let numPad = this.createNumPad();
+    let operPad = this.createOperPad();
 
     return (
       <View style={styles.btnContainer}>
@@ -85,7 +151,9 @@ class calculator extends Component {
     return (
       <View style={styles.calculatorWrapper}>
         <View style={styles.resultContainer}>
-          <Text style={styles.resultText}> {this.state.screen} </Text>
+          <Text style={styles.resultText}>
+            {this.state.screen === '' ? '0' : this.state.screen}
+          </Text>
         </View>
         <View style={styles.inputContainer}>{this.getLayout()}</View>
       </View>
